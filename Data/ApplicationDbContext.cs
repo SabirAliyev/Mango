f3d67c96142c937b085b1b151461ef17dda905ec
sabirali <sabir.aliyev.itdep@gmail.com>
2023-01-11 10:59:14 +0400

    first configuration of db context, no migrations added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Mango.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Data;
+
+public class ApplicationDbContext : DbContext
+{
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+	{
+
+	}
+
+	public DbSet<Category> Category { get; set; }
+}
---
+++ Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mango.Models;
+
+public class Category
+{
+    [Key]
+    public int Id { get; set; }
+
+    public int Name  { get; set; }
+
+    public string DisplayOrder { get; set; }
+}
