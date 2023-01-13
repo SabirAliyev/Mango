ee742dd403646a5cd15d92ab31fa90a51b357420
sabirali <sabir.aliyev.itdep@gmail.com>
2023-01-13 22:13:04 +0400

    build home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using Mango.Models;
+﻿using Mango.Data;
+using Mango.Models;
+using Mango.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace Mango.Controllers
@@ -7,15 +10,23 @@ namespace Mango.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeVM homeVM = new HomeVM()
+            {
+                Products = _db.Product.Include(u => u.Category),
+                Categories = _db.Category
+            };
+
+            return View(homeVM);
         }
 
         public IActionResult Privacy()
