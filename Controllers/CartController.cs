5bbd388f2105aa498c714cd0a379a654cc003f80
sabirali <sabir.aliyev.itdep@gmail.com>
2023-01-26 08:30:43 +0400

    cart - create Summary Get method in CartController

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,13 +1,21 @@
 ﻿using Mango.Data;
 using Mango.Models;
+using Mango.Models.ViewModels;
 using Mango.Utility; // using our own implementation
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Mango.Controllers;
 
 public class CartController : Controller
 {
     public readonly ApplicationDbContext _db;
+
+    /// <summary>
+    /// BindProperty allows us not to psecify this field in action methods as a parameter
+    /// </summary>
+    [BindProperty]
+    public ProductUserVM ProductUserVM { get; set; }
 
     public CartController(ApplicationDbContext db)
     {
@@ -50,4 +58,40 @@ public class CartController : Controller
         return RedirectToAction(nameof(Index));
     }
 
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    [ActionName(nameof(Index))]
+    public IActionResult IndexPost()
+    {
+        return RedirectToAction(nameof(Summary));
+    }
+
+    private object Summary()
+    {
+        ClaimsIdentity claimIdentity = (ClaimsIdentity)User.Identity;
+
+        // if User logged into the system we can get user identifer
+        Claim claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+        // another way go get user identity id
+        // string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        List<ShoppingCart> shoppingCarttList = new List<ShoppingCart>();
+        if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart) != null
+            && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).Count() > 0) {
+            shoppingCarttList = HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WebConstants.SessionCart).ToList();
+        }
+
+        List<int> prodsInCart = shoppingCarttList.Select(i => i.ProductId).ToList();
+        IEnumerable<Product> productList = _db.Product.Where(u => prodsInCart.Contains(u.Id));
+
+        // using claim object value we get identity information of logged user and put it into ProductUser View Model
+        ProductUserVM = new ProductUserVM()
+        {
+            ApplicationUser = _db.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+        };
+
+
+        return View(ProductUserVM);
+    }
 }
